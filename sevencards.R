2810c36f6be166e885aaecfa28e4b3143a0e637f
Vangie Shue <vangieshue@gmail.com>
2016-02-01 19:49:45 -0500

    Create sevencards.R

---
+++ sevencards.R
@@ -0,0 +1,11 @@
+n = 10 #number of cards of each type?
+cardvalues = c(1, 2, 4, 8, 16, 32, 64)
+numcards = length(cardvalues) #number of different card types
+cardcounts = rep(n, numcards)
+
+N = 21 #target score
+score = 0 #current score
+cardsdrawn = 0 #number of cards drawn so far?
+while(score < N) {
+  sample(1:7)
+}
